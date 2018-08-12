37f4f3b9d11ca49b85e6e491fbe6bb3f26fae7b5
deadpatron <deadpatron@gmail.com>
2018-08-13 01:11:19 +0300

    развернул рабочую область

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var menuIcon = document.querySelector(".header__button");
+var navigationList = document.querySelector(".navigation");
+
+var onMenuClick = function () {
+  navigationList.classList.add(".open-animation");
+}
